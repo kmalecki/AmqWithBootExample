20fc5e41d777d29212878c8b70468aa1bd22a87f
Krystian Małecki <krystianm1986@gmail.com>
2018-03-18 12:45:24 +0100

    Init commit

---
+++ MessageProducer/src/main/java/com/malecki/amqproducer/MessageProducerApplication.java
@@ -0,0 +1,12 @@
+package com.malecki.amqproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageProducerApplication.class, args);
+	}
+}
