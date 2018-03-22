e0bf3213e9854e12934530e8543a3a3142b19010
Krystian Małecki <krystianm1986@gmail.com>
2018-03-22 22:04:28 +0100

    Updated ProducerConfig.

--- MessageProducer/src/main/java/com/malecki/amqproducer/producer/ProducerConfig.java
+++ MessageProducer/src/main/java/com/malecki/amqproducer/producer/ProducerConfig.java
@@ -3,6 +3,7 @@ package com.malecki.amqproducer.producer;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
 
